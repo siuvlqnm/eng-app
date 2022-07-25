82035e0c0ccccb95ebcd8f989c98396ea305391d
siuvlqnm <siuvlqnm@gmail.com>
2022-07-25 11:51:42 +0800

    会员入场

---
+++ dist/js/gin-vue-admin-userEntryCard-legacy.16587209710002.js
@@ -0,0 +1,4 @@
+/*! 
+ Build based on gin-vue-admin 
+ Time : 1658720971000 */
+System.register(["../gva/gin-vue-admin-index-legacy.1658720971000.js"],(function(t){"use strict";var e;return{setters:[function(t){e=t.s}],execute:function(){t("c",(function(t){return e({url:"/sold/createUserEntryCard",method:"post",data:t})})),t("a",(function(t){return e({url:"/sold/deleteUserEntryCard",method:"delete",data:t})})),t("d",(function(t){return e({url:"/sold/deleteUserEntryCardByIds",method:"delete",data:t})})),t("u",(function(t){return e({url:"/sold/updateUserEntryCard",method:"put",data:t})})),t("f",(function(t){return e({url:"/sold/findUserEntryCard",method:"get",params:t})})),t("g",(function(t){return e({url:"/sold/getUserEntryCardList",method:"get",params:t})}))}}}));
--- dist/js/gin-vue-admin-userEntryCard.16587201700002.js
+++
@@ -1,4 +0,0 @@
-/*! 
- Build based on gin-vue-admin 
- Time : 1658720170000 */
-import{s as e}from"../gva/gin-vue-admin-index.1658720170000.js";const d=d=>e({url:"/sold/createUserEntryCard",method:"post",data:d}),t=d=>e({url:"/sold/deleteUserEntryCard",method:"delete",data:d}),a=d=>e({url:"/sold/deleteUserEntryCardByIds",method:"delete",data:d}),r=d=>e({url:"/sold/updateUserEntryCard",method:"put",data:d}),s=d=>e({url:"/sold/findUserEntryCard",method:"get",params:d}),o=d=>e({url:"/sold/getUserEntryCardList",method:"get",params:d});export{t as a,d as c,a as d,s as f,o as g,r as u};
